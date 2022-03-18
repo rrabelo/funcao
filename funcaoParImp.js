3e731aae0a0c2b57cf2d238cc6caec77378a3dd5
Rafael Rabelo <60629367+rrabelo@users.noreply.github.com>
2022-03-17 22:06:30 -0300

    Add files via upload

---
+++ funcaoParImp.js
@@ -0,0 +1,9 @@
+function parimpar(n){
+    if(n%2 == 0) {
+        console.log(`O número é par`)
+    } else {
+        console.log('Número impar')
+    }
+}
+
+let valor = parimpar(2)
---
+++ funcaoSoma.js
@@ -0,0 +1,8 @@
+function somando(n1=0,n2=0){
+
+    return n1 + n2
+
+}
+
+let soma = somando(1,10)
+console.log(`A soma dos parametros é ${soma}`)
